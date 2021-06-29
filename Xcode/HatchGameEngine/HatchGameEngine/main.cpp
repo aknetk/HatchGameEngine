45252671cc06273dfd519779392947063fe49b4b
Aurum <aurumdude@gmail.com>
2021-06-29 11:27:17 -0400

    Mac XCode 12.5 project

---
+++ Xcode/HatchGameEngine/HatchGameEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HatchGameEngine
+//
+//  Created by Justin on 6/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
