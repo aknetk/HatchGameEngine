5bd7ed5349d19a42bdff9a5133892f63fde467cb
Axanery <41282531+Axanery@users.noreply.github.com>
2022-09-06 01:46:28 -0400

    Add Music.PlayAtTime and Music.LoopAtTime functions

--- source/Engine/Bytecode/StandardLibrary.cpp
+++ source/Engine/Bytecode/StandardLibrary.cpp
@@ -3982,6 +3982,27 @@ VMValue Music_Play(int argCount, VMValue* args, Uint32 threadID) {
     return NULL_VAL;
 }
 /***
+ * Music.PlayAtTime
+ * \desc Places the music onto the music stack and plays it at a time (in seconds).
+ * \param music (Integer): The music index to play.
+ * \param startPoint (Decimal): The time (in seconds) to start the music at.
+ * \paramOpt panning (Decimal): Control the panning of the audio. -1.0 makes it sound in left ear only, 1.0 makes it sound in right ear, and closer to 0.0 centers it. (0.0 is the default.)
+ * \paramOpt speed (Decimal): Control the speed of the audio. > 1.0 makes it faster, < 1.0 is slower, 1.0 is normal speed. (1.0 is the default.)
+ * \paramOpt volume (Decimal): Controls the volume of the audio. 0.0 is muted, 1.0 is normal volume. (1.0 is the default.)
+ * \ns Music
+ */
+VMValue Music_PlayAtTime(int argCount, VMValue* args, Uint32 threadID) {
+    CHECK_AT_LEAST_ARGCOUNT(2);
+    ISound* audio = GET_ARG(0, GetMusic);
+    double start_point = GET_ARG(1, GetDecimal);
+    float panning = GET_ARG_OPT(1, GetDecimal, 0.0f);
+    float speed = GET_ARG_OPT(2, GetDecimal, 1.0f);
+    float volume = GET_ARG_OPT(3, GetDecimal, 1.0f);
+
+    AudioManager::PushMusicAt(audio, start_point, false, 0, panning, speed, volume);
+    return NULL_VAL;
+}
+/***
  * Music.Stop
  * \desc Removes the music from the music stack, stopping it if currently playing.
  * \param music (Integer): The music index to play.
@@ -4065,6 +4086,30 @@ VMValue Music_Loop(int argCount, VMValue* args, Uint32 threadID) {
     float volume = GET_ARG_OPT(5, GetDecimal, 1.0f);
 
     AudioManager::PushMusic(audio, true, loop_point, panning, speed, volume);
+    return NULL_VAL;
+}
+/***
+ * Music.LoopAtTime
+ * \desc Places the music onto the music stack and plays it, looping back to the specified sample index if it reaches the end of playback.
+ * \param music (Integer): The music index to play.
+ * \param loop (Boolean): Unused.
+ * \param loopPoint (Integer): The sample index to loop back to.
+ * \paramOpt panning (Decimal): Control the panning of the audio. -1.0 makes it sound in left ear only, 1.0 makes it sound in right ear, and closer to 0.0 centers it. (0.0 is the default.)
+ * \paramOpt speed (Decimal): Control the speed of the audio. > 1.0 makes it faster, < 1.0 is slower, 1.0 is normal speed. (1.0 is the default.)
+ * \paramOpt volume (Decimal): Controls the volume of the audio. 0.0 is muted, 1.0 is normal volume. (1.0 is the default.)
+ * \ns Music
+ */
+VMValue Music_LoopAtTime(int argCount, VMValue* args, Uint32 threadID) {
+    CHECK_AT_LEAST_ARGCOUNT(3);
+    ISound* audio = GET_ARG(0, GetMusic);
+    // int loop = GET_ARG(1, GetInteger);
+    double start_point = GET_ARG(1, GetDecimal);
+    int loop_point = GET_ARG(3, GetInteger);
+    float panning = GET_ARG_OPT(4, GetDecimal, 0.0f);
+    float speed = GET_ARG_OPT(5, GetDecimal, 1.0f);
+    float volume = GET_ARG_OPT(6, GetDecimal, 1.0f);
+
+    AudioManager::PushMusicAt(audio,start_point, true, loop_point, panning, speed, volume);
     return NULL_VAL;
 }
 /***
@@ -8160,12 +8205,14 @@ PUBLIC STATIC void StandardLibrary::Link() {
     // #region Music
     INIT_CLASS(Music);
     DEF_NATIVE(Music, Play);
+    DEF_NATIVE(Music, PlayAtTime);
     DEF_NATIVE(Music, Stop);
     DEF_NATIVE(Music, StopWithFadeOut);
     DEF_NATIVE(Music, Pause);
     DEF_NATIVE(Music, Resume);
     DEF_NATIVE(Music, Clear);
     DEF_NATIVE(Music, Loop);
+    DEF_NATIVE(Music, LoopAtTime);
     DEF_NATIVE(Music, IsPlaying);
     DEF_NATIVE(Music, GetPosition);
     DEF_NATIVE(Music, Alter);
